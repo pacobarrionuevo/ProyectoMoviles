449f5b67a63ca97af23323dbefbc03e9cf6fdf79
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-11-01 23:41:13 +0100

    Commit inicial
    Está hecho la conexion entre vistas, un poco de la pantalla principal y el archivo sobrenosotros

---
+++ AplicacionConciertos/app/src/main/java/com/example/aplicacionconciertos/SobreNosotros.kt
@@ -0,0 +1,9 @@
+package com.example.aplicacionconciertos
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SobreNosotrosScreen() {
+    Text(text = "Bienvenidos a la sección Sobre Nosotros.")
+}
---
+++ AplicacionConciertos/app/src/main/java/com/example/aplicacionconciertos/ui/theme/Type.kt
@@ -0,0 +1,62 @@
+package com.example.aplicacionconciertos.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.aplicacionconciertos.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val bodyFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Anek Kannada"),
+        fontProvider = provider,
+    )
+)
+
+val displayFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Anek Kannada"),
+        fontProvider = provider,
+    )
+)
+
+// Default Material 3 typography values
+val baseline = Typography(
+    bodyLarge = TextStyle(
+        fontSize = 32.sp
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 24.sp
+    )
+)
+
+val AppTypography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+)
+
