dcb467bed013072e4f2bb37ab3747c0998c9236d
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-11-28 15:32:32 +0100

    commit clase
    holaaaa

--- AplicacionConciertos/app/src/main/java/com/example/aplicacionconciertos/model/artists/DatosArtistas.kt
+++ AplicacionConciertos/app/src/main/java/com/example/aplicacionconciertos/model/artists/DatosArtistas.kt
@@ -1,2 +1,12 @@
 package com.example.aplicacionconciertos.model.artists
 
+data class DatosArtistas(
+    val id: Int,
+    val name: String,
+    val genres: List<String>,
+    val imageUrl: String,
+    val birthYear: Int,
+    val precioEntrada: Int, 
+    val concertLocations: List<String>,
+    val albums: List<String>
+)
