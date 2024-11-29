0e25d78058825a2b76e68be6c4ca8a187dcb9c3a
pacobarrionuevo <145661176+pacobarrionuevo@users.noreply.github.com>
2024-11-29 17:20:30 +0100

    prueba salguerinchi

--- AplicacionConciertos/app/src/main/java/com/example/aplicacionconciertos/model/artistas/DatosArtistas.kt
+++ AplicacionConciertos/app/src/main/java/com/example/aplicacionconciertos/model/artistas/DatosArtistas.kt
@@ -1,12 +1,12 @@
 package com.example.aplicacionconciertos.model.artistas
 
 data class DatosArtistas (
-    val id: String,
+    val id: Int,
     val name: String,
     val genres: List<String>,
     val image_url: String,
-    val birth_year: String,
-    val precio_entrada: String,
+    val birth_year: Int,
+    val precio_entrada: Int,
     val concert_locations: List<String>,
     val albums: List<String>
 )
